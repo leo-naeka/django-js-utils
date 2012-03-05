560b72577280a2637c0d9b6e8e6bf97d5f369f26
Raphael Lullis <raphael@lullis.net>
2012-03-05 15:55:26 -0300

    Adding setup.py to allow installation through setuptools/easy_install/pip. Adding django as only dependency.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+from sys import version_info
+from setuptools import setup
+
+fp = open(os.path.join(os.path.dirname(__file__), "README.rst"))
+readme_text = fp.read()
+fp.close()
+
+DESCRIPTION = 'Utility library to help development of RIA on top of a Django backend'
+
+setup(
+    name='django-js-utils',
+    author='Vebjorn Ljosa',
+    packages=['django_js_utils'],
+    version='0.0.1dev',
+    description=DESCRIPTION,
+    long_description=readme_text,
+    install_requires=['django'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ]
+)
